718858584437cf2d705953f5e8e03d651f9958ea
icejoywoo <icejoywoo@gmail.com>
2014-03-23 19:21:32 +0800

    add map

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 scons -Q
+if [ $? -ne 0 ]; then
+    echo "Failed to build!!!"
+    exit -1
+fi
 
 ./stl_unittest
+if [ $? -ne 0 ]; then
+    echo "Failed to run unittest!!!"
+    exit -1
+fi
--- test/map_test.cpp
+++ test/map_test.cpp
@@ -29,3 +29,65 @@ TEST_F(MapTest, IterateElements) {
     }
     EXPECT_EQ(100, m_map.size());
 }
+
+TEST_F(MapTest, InsertElements) {
+    std::map<int, int> id_mapping;
+    id_mapping.insert(std::make_pair(1, 1));
+    id_mapping.insert(std::make_pair(2, 2));
+    id_mapping.insert(std::make_pair(3, 3));
+    id_mapping.insert(std::make_pair(4, 4));
+    for (std::map<int, int>::iterator it = id_mapping.begin();
+            it != id_mapping.end();) {
+        printf("%d: %d\n", it->first, it->second);
+        if (it->first == 4) {
+            std::pair<std::map<int, int>::iterator, bool> ret = id_mapping.insert(std::make_pair(5, 5));
+            it = ret.first;
+        } else {
+            ++it;
+        }
+    }
+}
+
+TEST_F(MapTest, CustomKeyComparator) {
+    std::map<int, int, std::greater<int> > int_map;
+    typedef std::map<int, int, std::greater<int> >::iterator iterator_type;
+    for (int i = 0; i < 10; ++i) {
+        int_map.insert(std::make_pair(i, i));
+    }
+
+    int a = 9;
+    for (iterator_type it = int_map.begin(); it != int_map.end(); ++it) {
+        EXPECT_EQ(a, it->first);
+        EXPECT_EQ(a, it->second);
+        --a;
+    }
+}
+
+TEST_F(MapTest, EraseElement) {
+    m_map.erase(m_map.find(0));
+    EXPECT_EQ(99, m_map.size());
+
+    EXPECT_EQ(1, m_map.erase(1));
+    EXPECT_EQ(98, m_map.size());
+    m_map.erase(m_map.find(90), m_map.end());
+    EXPECT_EQ(88, m_map.size());
+    
+    EXPECT_EQ(0, m_map.count(1));
+    EXPECT_EQ(1, m_map.count(5));
+}
+
+TEST_F(MapTest, MultimapTest) {
+    std::multimap<int, int> multimap;
+    for (int i = 0; i < 10; ++i) {
+        multimap.insert(std::make_pair(i, i));
+        multimap.insert(std::make_pair(i, i));
+    }
+
+    EXPECT_EQ(2, multimap.erase(0));
+    typedef std::multimap<int, int>::iterator iterator_type;
+    for (iterator_type it = multimap.begin(); it != multimap.end();) {
+        printf("%d: %d\n", it->first, it->second);
+        it = multimap.erase(it);
+    }
+    
+}
