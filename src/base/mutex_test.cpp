48daef8fef470ba8cc3972d16e15682036baf1f4
icejoywoo <icejoywoo@gmail.com>
2014-04-27 00:50:36 +0800

    add simple mutex test

--- src/base/mutex_test.cpp
+++ src/base/mutex_test.cpp
@@ -1,9 +1,42 @@
+#include <pthread.h>
+#include <stdio.h>
+
 #include "mutex.h"
 
 using namespace icejoywoo::base;
 
+int global = 0;
+MutexLock mutex;
+
+void* test_function(void* arg) {
+    (void) arg; // unused
+    for (int i = 0; i < 20; ++i) {
+        {
+            MutexLockGuard guard(mutex);
+            ++global;
+            printf("global: %d\n", global);
+        }
+    }
+    return NULL;
+}
+
 int main() {
-    MutexLock lock;
-    MutexLockGuard guard(lock);
+    pthread_t thread;
+    if (pthread_create(&thread, NULL, test_function, NULL)) {
+        fprintf(stderr, "pthread join error.\n");
+        return -1;
+    }
+    for (int i = 0; i < 20; ++i) {
+        {
+            MutexLockGuard guard(mutex);
+            ++global;
+            printf("global: %d\n", global);
+        }
+    }
+    if (pthread_join(thread, NULL)) {
+        fprintf(stderr, "pthread join error.\n");
+        return -1;
+    }
+    printf("global: %d\n", global);
     return 0;
 }
